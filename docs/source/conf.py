774bbaab477e967db0bb865dc677f969ffd5e569
Peter McGill <18149966+astrophpeter@users.noreply.github.com>
2025-09-30 15:01:02 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,15 +3,19 @@ copyright = '2025, LLNL'
 author = 'LLNL'
 release = 'v3.0'
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_static_path = ['_static']
 
+
+
 extensions = [
     'sphinx_rtd_theme',
+    "sphinxcontrib.bibtex",
 ]
+
+bibtex_bibfiles = ["refs.bib"]
+bibtex_reference_style = "author_year"
 
 html_theme = "sphinx_rtd_theme"
